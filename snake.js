d7271b76618b564ba44d768702b4083ee38625cb
George Jindo <31431673+George-Jindo@users.noreply.github.com>
2019-05-21 07:41:11 -0700

    Reverted back to the old copy of the code

--- snake.js
+++ snake.js
@@ -1,26 +1,24 @@
 //sets the starting pixel for the snake & food
 let pixel = 30;
-        
+
 
 //creates snake array
 let snake = [];
 snake[0] = {
-    x: 15 * 5,
-    y: 16 * 5
+    x: 15 * pixel,
+    y: 16 * pixel
 }
 
 //next line allows the apple to be placed randomly on the canvas
 let apple = {
-    x: Math.floor(Math.random() * 20) * pixel,
+    x: Math.floor(Math.random() * 19) * pixel,
     y: Math.floor(Math.random() * 19) * pixel
 }
 
 //sets and creates the score variable
+
 let newScore = 0;
 let scoreText = "Score: ";
-
-//new snake speed used within a 30x30 grid
-let newSnakeSpeed = 5;
 
 let snakeDirection;
 
@@ -45,8 +43,8 @@ function direction(event) {
 
 
 function drawEverything() {
-    const ctx = document.getElementById("gameCanvas").getContext('2d');            
-    
+    const ctx = document.getElementById("gameCanvas").getContext('2d');
+
     ctx.fillStyle = "#2a2a2a";
     ctx.fillRect(0, 30, gameCanvas.width, gameCanvas.height);
     ctx.strokeStyle = "white";
@@ -54,17 +52,17 @@ function drawEverything() {
 
     //draws snake
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i == 0) ? "red" : "white";
-        ctx.fillRect(snake[i].x, snake[i].y +4.66, pixel, pixel);
+        ctx.fillStyle = (i == 0) ? "red" : "gray";
+        ctx.fillRect(snake[i].x, snake[i].y, pixel, pixel);
 
-        ctx.strokeStyle = (i ==0) ? "white" : "black";
-        ctx.strokeRect(snake[i].x, snake[i].y +4.66, pixel, pixel);
+        ctx.strokeStyle = (i == 0) ? "white" : "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, pixel, pixel);
     }
 
     //draws apple
     ctx.fillStyle = 'green';
     ctx.beginPath();
-    ctx.arc(apple.x + 15, apple.y + 15, 10, 0, Math.PI * 2, true);
+    ctx.arc(apple.x + 15, apple.y + 15, 15, 0, Math.PI * 2, true);
     ctx.fill();
 
     //old snake head
@@ -73,27 +71,21 @@ function drawEverything() {
 
 
     //moves snake according to which direction the user initiates
-    if (snakeDirection == "LEFT") snakeX -= 5;
-    if (snakeDirection == "UP") snakeY -= 5;
-    if (snakeDirection == "RIGHT") snakeX += 5;
-    if (snakeDirection == "DOWN") snakeY += 5;
+    if (snakeDirection == "LEFT") snakeX -= pixel;
+    if (snakeDirection == "UP") snakeY -= pixel;
+    if (snakeDirection == "RIGHT") snakeX += pixel;
+    if (snakeDirection == "DOWN") snakeY += pixel;
 
     if (Math.abs(snakeX - apple.x) < pixel && Math.abs(snakeY - apple.y) < pixel) {
         newScore++;
         apple = {
-            x: Math.floor(Math.random() * 19 + 1) * pixel,
-            y: Math.floor(Math.random() * 18 + 1) * pixel
+            x: Math.floor(Math.random() * 19) * pixel,
+            y: Math.floor(Math.random() * 19) * pixel
         }
     } else {
         //remove the tail
         snake.pop();
     }
-
-    //collision detection
-    if (snakeX < 0 || snakeX > gameCanvas.width - 30 || snakeY < 0 + 30 || snakeY > gameCanvas.height - 30) {
-        clearInterval(game);
-    }
-    
 
     //new head creation
     let newHead = {
@@ -103,17 +95,23 @@ function drawEverything() {
 
     //creates the new head
     snake.unshift(newHead);
-    
+
+    //cnvas collision detection
+    if (snakeX < 0 || snakeX > gameCanvas.width - pixel || snakeY < 0 + 30 || snakeY > gameCanvas.height - pixel) {
+        clearInterval(game);
+    }
+
+   
+
     ctx.fillStyle = "#2a2a2a";
-    ctx.fillRect(0, 0, 600, 30);            
+    ctx.fillRect(0, 0, 600, 30);
 
     ctx.fillStyle = "green";
-    ctx.font = "20px arial";
-    ctx.fillText(scoreText + newScore, pixel / 2, pixel / 1.5);
-    requestAnimationFrame(drawEverything)   
+    ctx.font = "15px arial";
+    ctx.fillText(scoreText + newScore, 1, pixel / 2);
+
 }
-drawEverything();
+
 //calls draw function
 //let framesPerSecond = 30;
-//let game = setInterval(drawEverything, 1500 / framesPerSecond);
-
+let game = setInterval(drawEverything, 250);
